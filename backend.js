86eab4046661d635a90983a1fa02d8056d2ff557
mchlltt <me@northwestern.edu>
2016-11-05 13:58:45 -0500

    Add begin/end year and response number fields, use begin/end year fields

--- backend.js
+++ backend.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
 
-    // Built by LucyBot. www.lucybot.com
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-        'api-key': "2434876726a64f759a34973f3c928f7e",
-        'q': "placeholder"
+    var q;
+    var beginYear = '1900';
+    var endYear = '2017';
+
+    $('#search-button').on('click', function() {
+        q = $('#search-term').val();
+        console.log($('#begin-year').val());
+        if ($('#begin-year').val() !== '') {
+            beginYear = $('#begin-year').val();
+        }
+        if ($('#end-year').val() !== '') {
+            endYear = $('#end-year').val();
+        }
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "2434876726a64f759a34973f3c928f7e",
+            'q': q,
+            'begin_date': beginYear + '0101',
+            'end_date': endYear + '0101'
+        });
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(result) {
+            console.log(result);
+        }).fail(function(err) {
+            throw err;
+        });
     });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function(result) {
-        console.log(result);
-    }).fail(function(err) {
-        throw err;
-    });
+
 
 });
